b313048f91ea137e47f7797394b0f4920001e973
THE-ONE <lwyx12620@163.com>
2026-09-27 00:56:29 +0800

    Loading game background

--- Tetris.cpp
+++ Tetris.cpp
@@ -2,6 +2,9 @@
 #include <time.h>
 #include <stdlib.h>
 
+const int SPEED_NORMAL = 500;
+const int SPEED_FAST = 50;
+
 Tetris::Tetris(int rows, int cols, int left, int top, int BlockSize)
 {
 	this->rows = rows;
@@ -24,22 +27,48 @@ Tetris::Tetris(int rows, int cols, int left, int top, int BlockSize)
 
 void Tetris::init()
 {
-	delay = 30;
+	delay = SPEED_NORMAL;
 
 	srand(time(NULL));
+	initgraph(938, 896);
+	loadimage(&ImgBackground, "res/bg2.png");
+
+	char data[20][10];
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			data[i][j] = 0;
+		}
+	}
 }
 
 void Tetris::keyEvent()
 {
+
 }
 
 void Tetris::UpdateWindow()
 {
+	putimage(0, 0, &ImgBackground);
 }
 
 int Tetris::GetDelay()
 {
-	return 0;
+	static unsigned long long LastTime = 0;
+	unsigned long long CurrentTime= GetTickCount();
+	
+	if(LastTime== 0)
+	{
+		LastTime = CurrentTime;
+		return 0;
+	}
+	else
+	{
+		int ret = CurrentTime - LastTime;
+		LastTime = CurrentTime;
+		return ret;
+	}
 }
 
 void Tetris::drop()
